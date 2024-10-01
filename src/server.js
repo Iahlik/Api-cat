f02eaffb2829f3179de5c8ebc83ff2b0fc8f3ac8
Ignacio Larrain <ignaciolarrainv@gmail.com>
2024-10-01 12:31:30 -0300

    personalizacion de botones

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware para servir archivos estáticos desde la carpeta 'public'
+// Middleware
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Iniciar el servidor
+// Server Startup
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
